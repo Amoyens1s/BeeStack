00c613d2dc2e84c0c5c466125e99921f8f320c34
Amoyensis <amoyensis@outlook.com>
2021-10-29 22:01:47 +0800

    实现内存使用率时序数据采集以及graphql接口

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Beestack Server
+[![CodeFactor](https://www.codefactor.io/repository/github/beestacks/server/badge)](https://www.codefactor.io/repository/github/beestacks/server)
--- src/memory/memory.resolver.ts
+++ src/memory/memory.resolver.ts
@@ -1,4 +1,5 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
+import { MemoryTimeSeries } from './memory.interface';
 import { MemoryService } from './memory.service';
 
 @Resolver('Memory')
@@ -7,5 +8,23 @@ export class MemoryResolver {
   @Query('memory')
   async getMemory() {
     return this.memoryService.getMemory();
+  }
+
+  @Query('memoryTimeSeries')
+  async getMemoryTimeSeries(
+    @Args('from') from: number,
+    @Args('to') to: number,
+  ) {
+    const res = await this.memoryService.getMemoryTimeSeries(from, to);
+    const result: MemoryTimeSeries[] = [];
+    res.forEach((item) => {
+      result.push({
+        timestamp: item.timestamp,
+        usage: item.usage,
+        used: item.used,
+        total: item.total,
+      });
+    });
+    return result;
   }
 }
